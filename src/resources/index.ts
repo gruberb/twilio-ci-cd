0cd4df7175a43b8acc69a1c0d93c76868c7883bc
Bastian Gruber <foreach@me.com>
2021-03-03 19:17:14 +0100

    Add sound-notification plugin

---
+++ src/resources/index.ts
@@ -0,0 +1,45 @@
+import { CheckServerless, Resource, Serverless, FlexPlugin, Studio } from 'twilio-pulumi-provider';
+import * as pulumi from '@pulumi/pulumi';
+
+const stack = pulumi.getStack();
+
+const serviceName = 'example-serverless';
+const domain = CheckServerless.getDomainName(serviceName, stack);
+
+const flexWorkspace = new Resource("flex-workspace", {
+  resource: ["taskrouter", "workspaces"],
+  attributes: {
+      sid: process.env.FLEX_WORKFLOW_SID,
+  }
+});
+
+const englishTaskQueue = new Resource("english-taskQueue", {
+  resource: ["taskrouter", { "workspaces" : flexWorkspace.id }, "taskQueues"],
+  attributes: {
+      targetWorkers: `languages HAS "english"`,
+      friendlyName: 'English Queue',
+  }
+});
+
+const serverless = new Serverless("example-functions-assets", {
+  attributes: {
+      cwd: `../serverless/main`,
+      serviceName,          
+      envPath: `.${stack}.env`,
+      functionsEnv: stack,
+      pkgJson: require("../serverless/main/package.json")
+  }
+});
+
+
+const soundNotificationFlexPlugin = new FlexPlugin("example-sound-notification-flex-plugin", { 
+  attributes: {
+      cwd: "../flex-plugins/plugin-sound-notification",
+      env: pulumi.all([domain]).apply(([ domain ]) => (
+          {
+              REACT_APP_SERVERLESSassets_DOMAIN_NAME: domain
+          }
+      )),
+      runTestsOnPreview: true
+  }
+});
