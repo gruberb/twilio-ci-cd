49c414dfaaf8e7393c3e830a0f6be129cd103ea9
Bastian Gruber <foreach@me.com>
2021-03-05 16:04:36 +0100

    Add new resources

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -13,11 +13,53 @@ const flexWorkspace = new Resource("flex-workspace", {
   }
 });
 
-const englishTaskQueue = new Resource("english-taskQueue", {
+const anyoneTaskQueue = new Resource("eveyone-taskQueue", {
   resource: ["taskrouter", { "workspaces" : flexWorkspace.id }, "taskQueues"],
   attributes: {
-      targetWorkers: `languages HAS "english"`,
-      friendlyName: 'English Queue',
+      targetWorkers: `1==1`,
+      friendlyName: 'Everyone',
+  }
+});
+
+const workflow = new Resource("anyone", {
+  resource: ["taskrouter", { "workspaces" : flexWorkspace.sid }, "workflows"],
+  attributes: {
+      friendlyName: 'Assign to Anyone',
+      configuration: pulumi.all([anyoneTaskQueue.sid])
+            .apply(([ anyoneTaskQueueSID ]) => JSON.stringify(
+                {
+                    task_routing: {
+                        filters: [],
+                        default_filter: {
+                          queue: anyoneTaskQueueSID
+                        }
+                    }
+                }
+            ))
+  },
+});
+
+const environmentFlow = new Resource("example-environment-studio", {
+  resource: ["studio", "flows"],
+  attributes: {
+      commitMessage: "Release of revision 18", 
+      friendlyName: 'A New Flow v6',
+      status: 'published',
+      definition: pulumi.all([workflow.sid]).apply(([workflowSid]) => 
+          Studio.getStudioFlowDefinition({
+            pathToFlow: "../studio-flows/webchat_flow.json",
+          },{
+              transformations: [
+                  {
+                      name: "changeWidgetProps",
+                      types: ["send-to-flex"],
+                      exec: props => {
+                          props.properties.workflow =  workflowSid
+                      }
+                  },
+              ]
+          })
+      )
   }
 });
 
@@ -36,6 +78,7 @@ const serverless = new Serverless("functions-assets", {
 const soundNotificationFlexPlugin = new FlexPlugin("sound-notification-flex-plugin", { 
   attributes: {
       cwd: "../flex-plugins/plugin-sound-notification",
+      serviceName,
       env: pulumi.all([domain]).apply(([ domain ]) => (
           {
               REACT_APP_SERVERLESSassets_DOMAIN_NAME: domain
